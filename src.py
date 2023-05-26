959829f78565e05337486405cf448c7f2ffff6a1
Juan <juanjulionn@gmail.com>
2023-05-26 12:03:20 +0200

    funciones de limpieza de os y pantalla

--- src.py
+++ src.py
@@ -1,21 +1,19 @@
 def limpiar_os(col):
     return col.apply(lambda x: 1 if x == "Windows" else 0) #función muy sencilla para limpiar el OS
 
-
 def limpiar_sc(col):
-    for v in col: 
-        res = []
+    res = []
+    for v in col:
+        cleaned_values = []
         for l in v:
-            if l.isdigit(): 
-                res.append(l):
+            if l.isdigit():
+                cleaned_values.append(l)
+        res.append(cleaned_values)
 
-            else: 
-                pass 
-    
-        g1 = res[:3]
-        g2 = res[4:]
-    
-    return col.apply(lambda x:g1*g2)
+    g1 = [''.join(x[:3]) for x in res]  # Sacamos los primeros 4 dígitos y los unimos para multiplicar
+    g2 = [''.join(x[4:]) for x in res]  #Pillamos el resto y hacemos otro grupo
 
+    result = [int(x) * int(y) for x, y in zip(g1, g2)]  # Multiplicacmos los elementos tras convertirlos a int
 
+    return result
 
